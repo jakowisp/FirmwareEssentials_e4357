109af61b27e96a3e5d2b92f7983ea039ee6f3824
jakowisp <ddilbeck@yahoo.com>
2015-04-20 00:28:06 -0400

    Update Hw1 to perform the toglle faster by using SET and CLR

--- hw1/main.c
+++ hw1/main.c
@@ -7,13 +7,18 @@
 // as pointers to volatile data
 
 #define FIO2DIR0  (*(volatile unsigned char *)(0x2009c040))
+#define FIO2SET   (*(volatile unsigned char *)(0x2009c058))
+#define FIO2CLR   (*(volatile unsigned char *)(0x2009c05c))
 #define FIO2PIN0  (*(volatile unsigned char *)(0x2009c054))
 
 int main() {
-   FIO2DIR0=0x20;
+   register const unsigned char mask = 0x20;
+   FIO2DIR0=mask;
    while(1) {
-       FIO2PIN0 |= 0x20;
-       FIO2PIN0 &= ~0x20;
+       //FIO2PIN0 |= 0x20;
+       //FIO2PIN0 &= ~0x20;
+       FIO2SET = mask;
+       FIO2CLR = mask;
    }
 }
 
