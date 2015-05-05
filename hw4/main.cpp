4cc703d253645f04a2d470757dd416fa7fb74cd6
jakowisp <ddilbeck@yahoo.com>
2015-05-04 21:57:35 -0400

    Add PWM1 and 3 outputs to be routed to the DMA enables

---
+++ hw4/main.cpp
@@ -0,0 +1,118 @@
+/* THIS SOFTWARE IS PROVIDED "AS IS".  NO WARRANTIES, WHETHER EXPRESS, IMPLIED
+ * OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE.
+ * THE AUTHORS SHALL NOT, IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL, OR
+ * CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER. USE IT AT YOUR OWN RISK */
+
+#include "mbed.h"
+
+unsigned char    sample0[16*1024] __attribute__((section("AHBSRAM1")));
+unsigned char    sample1[16*1024] __attribute__((section("AHBSRAM0")));
+void DMAInit();
+void setupPWM1(void);
+
+
+int main (void) 
+{
+	
+	// GPIO config
+	LPC_GPIO2->FIODIR0 = 0xFF;
+	LPC_GPIO2->FIOPIN0 = 1;
+	
+	// setup timer1
+	LPC_SC->PCONP |= 1 << 2; // Power up
+	LPC_SC->PCLKSEL0 |= 0x01 << 4; // CCLK
+	LPC_TIM1->MR0 = 1 << 4 ;
+	LPC_TIM1->MCR = 1 << 1; // reset on Match Compare 0
+		
+	uint32_t i;
+	
+	for(i=0;i<1024;i+=2)
+	{
+		sample0[i] = 0xFF;
+		sample0[i+1] = 0;
+	}
+
+	DMAInit();
+	
+	LPC_TIM1->IR |= 0xff; // Clear all timer interrupts if there are any
+	LPC_TIM1->TCR = 0x01; // start timer.
+	
+	while(1)
+	{
+        
+	}
+	return 0;
+  
+}
+
+
+void DMAInit()
+{
+	LPC_SC->PCONP |= 1 << 29; // Power up DMA
+	
+	LPC_GPDMACH0->DMACCConfig = 0; // stop ch0 dma
+	
+	LPC_GPDMA->DMACConfig |= 1 << 0; // enable DMA
+	LPC_GPDMA->DMACSync |= 1 << 10; // use MAT1.0 for Sync
+	LPC_SC->DMAREQSEL |= 1 << 2; // Timer1 Match Compare 0 as DMA request
+	
+	LPC_GPDMA->DMACIntErrClr |= 0xff;
+	LPC_GPDMA->DMACIntTCClear |= 0xff;
+		
+	LPC_GPDMACH0->DMACCDestAddr = (uint32_t) &(LPC_GPIO2->FIOPIN0); // LED is connected to P1.29.
+        // the array where alternating 1's and 0's. are stored
+        LPC_GPDMACH0->DMACCSrcAddr = (uint32_t) &sample0[0]; 
+	LPC_GPDMACH0->DMACCLLI = 0;
+	
+	LPC_GPDMACH0->DMACCControl = 1024 | (6 << 12) |  (6 << 15) |( 1 << 26 ); // Transfer size = 200 bytes and enable source increment.
+	
+	LPC_GPDMACH0->DMACCConfig = ( 10 << 6 ) | ( 1 << 11); // Set MAT1.0 as destination request peripheral and the type pf transfer as Memory to Peripheral.
+	
+	LPC_GPDMACH0->DMACCConfig |= 1; //enable ch0
+}
+
+
+void setupPWM1(void){
+    // ensure PWM peripheral is powered on (it is powered on by default)
+    LPC_SC->PCONP |= 1 << 6;
+    // Select SystemClock as PCLK. By default after reset PCLK = CLK/4
+    // [PCLKSEL0[13:12] = 2'b00 selects CCLK.  Table 42 of user manual
+    LPC_SC->PCLKSEL0 |= 1<<12;
+ 
+    //Keep the PWM halted while setting up configuration
+    LPC_PWM1->TCR = 2;                      // bring PWM module into reset
+    LPC_PWM1->IR = 0xff;                    // clear any pending interrupts
+    
+    // configure P2.0 for PWM1.1 - O - Pulse Width Modulator 1, channel 1 output.
+    LPC_PINCON->PINSEL4 = (LPC_PINCON->PINSEL4 & ~(0x3 << 0)) | (0x1 << 0);
+    LPC_PINCON->PINSEL4 = (LPC_PINCON->PINSEL4 & ~(0x3 << 4)) | (0x1 << 4);
+     
+    // Disable pullups for P2.0
+    LPC_PINCON->PINMODE4 = (LPC_PINCON->PINMODE4 & ~0x3 << 0) | (0x2 << 0);
+    LPC_PINCON->PINMODE4 = (LPC_PINCON->PINMODE4 & ~0x3 << 4) | (0x2 << 4);
+ 	
+    // Set Prescale to divide CCLK by 24
+    // 96MHZ/24= 4Mhz
+    // TC is incremented every PR+1 value. Default is 0.
+    //LPC_PWM1->PR = 23; 
+    // Set the period for all PWM1 signals
+    LPC_PWM1->MR0 = 512;             
+    // Set the high time of the period
+    // In this case 50/50
+    LPC_PWM1->MR1 = 256;
+
+    LPC_PWM1->MR3 = 256;
+    LPC_PWM1->MR4 = 512;              
+    //Set this value to ensure the changes are loaded into the PWM module
+    LPC_PWM1->LER = 0x7;		      
+ 
+    //Starts the PWM1 module
+    LPC_PWM1->MCR = 0x2;               // reset on MR0
+    // enable PWM1 with single-edge operation
+    // enable PWM3
+    // Configure PWM3 for dual edge operation
+    LPC_PWM1->PCR = 1<<11| 1 << 9 | 1<<3;            
+    LPC_PWM1->TCR = (1 << 3) | 1;      // enable PWM mode and counting
+}
+
